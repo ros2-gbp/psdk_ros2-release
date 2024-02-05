62d31acc75340e46e7b67a11bc3130a3f11e1e19
vicmassy <victor@unmanned.life>
2024-02-05 12:45:21 +0100

    Revert "Trimmed the branch to only the psdk_interfaces sub directory"
    This reverts commit 95ff87e137c7cfa68450f8f1d1dba535934a3885.

---
+++ psdk_wrapper/src/main.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2023 Unmanned Life
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * @file main.cpp
+ *
+ * @brief Main file which spins the PSDKWrapper class
+ *
+ * @author Bianca Bendris
+ * Contact: bianca@unmanned.life
+ *
+ */
+#include "psdk_wrapper/psdk_wrapper.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+std::shared_ptr<psdk_ros2::PSDKWrapper> psdk_ros2::global_ptr_;
+int
+main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+  auto psdk_node = std::make_shared<psdk_ros2::PSDKWrapper>("psdk_node");
+
+  psdk_ros2::global_ptr_ = psdk_node;
+  rclcpp::spin(psdk_node->get_node_base_interface());
+
+  rclcpp::shutdown();
+
+  return 0;
+}
